7b131581fb5473530c6b1f86eab910f7a79a1571
xxenyam <popova072005@gmail.com>
2024-10-03 20:23:44 +0200

    initial

---
+++ pattern.rb
@@ -0,0 +1,54 @@
+# Клас Посередник
+class Mediator
+  def initialize
+    @components = []
+  end
+
+  def add_component(component)
+    @components << component
+    component.mediator = self
+  end
+
+  # Метод для спілкування між компонентами
+  def notify(sender, event)
+    @components.each do |component|
+      next if component == sender
+      component.receive(event)
+    end
+  end
+end
+
+# Базовий компонент, що взаємодіє з посередником
+class Component
+  attr_accessor :mediator
+
+  def initialize(name)
+    @name = name
+  end
+
+  def send(event)
+    puts "#{@name} відправляє повідомлення: #{event}"
+    @mediator.notify(self, event)
+  end
+
+  def receive(event)
+    puts "#{@name} отримав повідомлення: #{event}"
+  end
+end
+
+# Використання
+mediator = Mediator.new
+
+component1 = Component.new("Компонент 1")
+component2 = Component.new("Компонент 2")
+component3 = Component.new("Компонент 3")
+component4 = Component.new("Компонент 4")
+
+mediator.add_component(component1)
+mediator.add_component(component2)
+mediator.add_component(component3)
+mediator.add_component(component4)
+
+component3.send("Привіт, Кс31!") 
+
+
